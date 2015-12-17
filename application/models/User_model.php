6d2536c71e8694d96e3df9a988537f23b872741c
Robert Reynolds <rob@arknetmedia.com>
2015-12-17 11:31:02 -0500

    master admin test

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -14,8 +14,8 @@ Class User_model extends CI_Model{
     }
     
     
-    function deleteUserInfo($id){
-        $this->db->delete('test', array('id' => $id));
+    function deleteUserInfoTest($id){
+        $this->db->delete('admin', array('id' => $id));
         
         return $this->db->affected_rows();
     }
